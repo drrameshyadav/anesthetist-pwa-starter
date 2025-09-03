6b9c516b2a21890dcc0506156c84f5aa77780a1e
drrameshyadav <ramesh.rinku@gmail.com>
2025-09-03 14:39:43 +0530

    Update CardGrid.tsx

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,5 +1,13 @@
-<div className="px-2 sm:px-4 md:px-6 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-  {cards.map((card, index) => (
-    <Card key={index} {...card} />
-  ))}
-</div>
+import cards from "../data/cards";
+import Card from "./Card";
+import { CardData } from "../types";
+
+export default function CardGrid() {
+  return (
+    <div className="px-4 sm:px-6 md:px-12 lg:px-24 grid gap-6 sm:grid-cols-2 md:grid-cols-3">
+      {cards.map((card: CardData, index: number) => (
+        <Card key={index} {...card} />
+      ))}
+    </div>
+  );
+}
